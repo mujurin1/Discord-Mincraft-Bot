2a458589dcd4efca412b2acdfd70a9f0af43633e
mujurin1 <mujurin25@yahoo.co.jp>
2021-11-07 02:10:17 +0900

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using MinecraftConnection;
+using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Program
+{
+    static string address = "127.0.0.1";
+    static ushort port = 25575;
+    static string pass = "rcon_pass";
+    static MinecraftCommands command = new MinecraftCommands(address, port, pass);
+
+    public static async Task Main()
+    {
+        var res = command.SendCommand("list");
+        Console.WriteLine(res);
+    }
+}
